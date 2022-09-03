faa100fd8a350b620bdc12209e67ada5a2159d91
sankaire <kerenketepela@gmail.com>
2022-09-03 23:00:03 +0300

    feat:authentication

---
+++ pages/api/signin.ts
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email: email } });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        time: Date.now(),
+      },
+      "tepela",
+      { expiresIn: "8h" }
+    );
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("Trax_acces_token", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    res.json(user);
+  } else {
+    res.status(401).json({ error: "wrong credentials" });
+  }
+};
